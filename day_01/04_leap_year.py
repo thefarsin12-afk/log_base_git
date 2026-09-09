ef0b224867dd34446d04ae4906e9ef6203c21931
Muhammed Farsin M <thefarsin12@gmail.com>
2026-09-09 12:55:17 +0530

    leap-year

--- day_01/04_leap_year.py
+++ day_01/04_leap_year.py
@@ -2,3 +2,12 @@
 write a program to chk year is leap year or not
 
 """
+
+def leap_year(year):
+
+    if year % 100 == 0 and year % 400 == 0 or year % 100 != 0 and year % 4 == 0:
+        print(f"Leap Year {year}")
+
+    else:print(f"Not Leap Year {year}")    
+
+leap_year(2021)
