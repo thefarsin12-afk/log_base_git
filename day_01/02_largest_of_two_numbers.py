0bff821bdf9daedb5cecf3d93806c68c2a57181d
Muhammed Farsin M <thefarsin12@gmail.com>
2026-09-09 12:48:27 +0530

    taskfile

--- day_01/02_largest_of_two_numbers.py
+++ day_01/02_largest_of_two_numbers.py
@@ -2,3 +2,12 @@
 write a program to chk largest of two numbers
 
 """
+
+def largest_number(num1,num2):
+
+    if num1 > num2:
+        print(f"largest number = {num1}")
+
+    else:print(f"largest number = {num2}")   
+
+largest_number(30,10)     
