f391db42151f2f2412deb6775b082cb4c60d561e
Muhammed Farsin M <thefarsin12@gmail.com>
2026-09-09 13:00:03 +0530

    largest-three-numbers

--- day_01/05_max_three.py
+++ day_01/05_max_three.py
@@ -4,3 +4,15 @@
 write a program to print largest of three numbers
 
 """
+
+def max_of_numbers(num1,num2,num3):
+
+    if num1 > num2 and num1 > num3:
+        print(f"Largest Number = {num1}")
+
+    elif num2 > num1 and num2 > num3:
+        print(f"Largest Number = {num2}")
+
+    else:print(f"Largest Number = {num3}")        
+
+max_of_numbers(10,500,100)
